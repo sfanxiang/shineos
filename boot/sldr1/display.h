ba63e2a43e2a015d6e39c6a6419e65f147219e2e
sfanxiang <sfanxiang@gmail.com>
2014-07-22 14:22:55 +0800

    fixed a mistake

--- boot/sldr1/display.h
+++ boot/sldr1/display.h
@@ -5,11 +5,13 @@ void _putchar(char chr)
 	asm("\
 		push bp\n\
 		mov bp,sp\n\
+		push bx\n\
 		sseg\n\
 		mov al,4[bp]\n\
 		mov bx,#7\n\
 		mov ah,#0xe\n\
 		int #0x10\n\
+		pop bx\n\
 		pop bp\
 	");
 }
