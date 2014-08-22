1738cf46f3daefbc778680c2d6d866c33aee3e7f
sfanxiang <sfanxiang@gmail.com>
2014-08-22 11:10:22 +0800

    switch back to bcc

--- boot/sldr1/cstart.asm
+++
@@ -1,5 +0,0 @@
-	global _cstart_
-_cstart_:
-	extern main_
-	call main_-1
-	jmp $
--- boot/sldr1/types.h
+++ boot/sldr1/types.h
@@ -1,9 +1,10 @@
 #ifndef _TYPES_H
 #define _TYPES_H
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned long u32;
+#include <asm/types.h>
+
+typedef __u8 u8;
+typedef __u16 u16;
+typedef __u32 u32;
 
 #endif
-
