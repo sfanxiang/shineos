9c2a3728c164d484c7b9772cbd7e7d0c6f9316c6
sfanxiang <sfanxiang@gmail.com>
2014-10-06 15:39:34 +0800

    writefs: fix typo

--- tools/writefs/main.cc
+++ tools/writefs/main.cc
@@ -11,7 +11,7 @@ int main(int argc,char *argv[])
 	{
 		cout<<"\
 Too few argument(s).\n\
-Usage: <target> <dir> <partation index> <block size> <part size>\n";
+Usage: <target> <dir> <partition index> <block size> <part size>\n";
 		return 1;
 	}
 	else
