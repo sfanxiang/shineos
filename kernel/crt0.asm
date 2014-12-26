fb948018cf3684b7c9f28045feb2ac9ef4d40ef9
sfanxiang <sfanxiang@gmail.com>
2014-12-26 21:12:48 +0800

    kernel: calculate size

--- kernel/crt0.asm
+++ kernel/crt0.asm
@@ -3,8 +3,18 @@
 
 .section .text
 
+.extern _end
+
+.globl kernel_size
+kernel_size:
+	.quad _end-_start
+
 .globl _start
 _start:
-	mov rsp,0x70000
+	lea rsp,stack_top[rip]
 	call _init
 	jmp kmain
+
+.section .stack,"aw",@nobits
+.skip 65536
+stack_top:
